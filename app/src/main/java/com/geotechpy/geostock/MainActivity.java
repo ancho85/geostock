2ccc47ca7a0de241a57e7bded4b2753b0e3c3f9a
Carlos Gomez <ancho.corp@gmail.com>
2015-06-28 17:15:47 -0400

    confirm dialog for database reset

--- app/src/main/java/com/geotechpy/geostock/MainActivity.java
+++ app/src/main/java/com/geotechpy/geostock/MainActivity.java
@@ -1,6 +1,11 @@
 package com.geotechpy.geostock;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -48,6 +53,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickReset(View view) {
-        Toast.makeText(MainActivity.this, R.string.dbreset, Toast.LENGTH_LONG).show();
+        FragmentManager fragmentManager = MainActivity.this.getSupportFragmentManager();
+        ConfirmDialog confirmDialog = new ConfirmDialog();
+        confirmDialog.show(fragmentManager, "");
+    }
+
+    public static class ConfirmDialog extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            builder.setMessage(R.string.confirm_action);
+            builder.setTitle(R.string.confirm_title);
+            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    Toast.makeText(getActivity(), R.string.dbreset, Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            return builder.create();
+        }
     }
 }
