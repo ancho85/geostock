f6b39be218eea55f63b2162f05e8ac8a09c073ee
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2015-06-29 16:31:36 -0400

    foreign keys enabled in sqlite

--- app/src/main/java/com/geotechpy/geostock/database/DBHelper.java
+++ app/src/main/java/com/geotechpy/geostock/database/DBHelper.java
@@ -64,6 +64,15 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
+
+    @Override
     public void onCreate(SQLiteDatabase db) {
         try {
             db.execSQL(sqlUserCreate);
