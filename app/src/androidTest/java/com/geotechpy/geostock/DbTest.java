0732276087848d88d9010b8b0525db8ca37a4481
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2015-06-29 14:45:20 -0400

    tests updated stockDetail tests added

--- app/src/androidTest/java/com/geotechpy/geostock/DbTest.java
+++ app/src/androidTest/java/com/geotechpy/geostock/DbTest.java
@@ -10,6 +10,7 @@ import com.geotechpy.geostock.database.StockManager;
 import com.geotechpy.geostock.database.UserManager;
 import com.geotechpy.geostock.database.ZoneManager;
 import com.geotechpy.geostock.models.Item;
+import com.geotechpy.geostock.models.Stock;
 import com.geotechpy.geostock.models.StockDetail;
 import com.geotechpy.geostock.models.User;
 import com.geotechpy.geostock.models.Zone;
@@ -39,13 +40,13 @@ public class DbTest extends AndroidTestCase {
     }
 
     public void testUser() throws Exception {
-        user.insert("ancho", "123456", "A");
-        user.insert("alex", "654321", "D");
-        user.insert("liz", "111222", "L");
-        user.insert("ondina", "333444", "Y");
+        user.insert("ancho", "123456", String.valueOf(R.string.zone_admin));
+        user.insert("alex", "654321", String.valueOf(R.string.zone_deposit));
+        user.insert("liz", "111222", String.valueOf(R.string.zone_lab));
+        user.insert("ondina", "333444", String.valueOf(R.string.zone_both));
         assertEquals(4, user.count().intValue());
 
-        user.update("ancho", "666", "A");
+        user.update("ancho", "666", String.valueOf(R.string.zone_admin));
         User ancho = UserManager.load(getContext(), "ancho");
         assertEquals("666", ancho.getPassword());
 
@@ -54,49 +55,67 @@ public class DbTest extends AndroidTestCase {
     }
 
     public void testZone() throws Exception {
-        zone.insert(1, "Admin", "A");
-        zone.insert(2, "Deposit", "D");
-        zone.insert(3, "Lab", "L");
-        zone.insert(4, "Deposit and Lab", "Y");
+        zone.insert(1, "Admin", String.valueOf(R.string.zone_admin));
+        zone.insert(2, "Deposit", String.valueOf(R.string.zone_deposit));
+        zone.insert(3, "Lab", String.valueOf(R.string.zone_lab));
+        zone.insert(4, "Deposit and Lab", String.valueOf(R.string.zone_both));
         assertEquals(4, zone.count().intValue());
 
-        zone.update(4, "Deposit & Lab", "Y");
-        Zone lab = ZoneManager.load(getContext(), 3);
-        assertEquals("L", lab.getType());
+        zone.update(4, "Deposit & Lab", String.valueOf(R.string.zone_both));
+        Zone lab = ZoneManager.load(getContext(), 4);
+        assertEquals(String.valueOf(R.string.zone_both), lab.getType());
 
         zone.delete(4);
         assertEquals(3, zone.count().intValue());
     }
 
     public void testItem() throws Exception {
-        item.insert("keyb", "Keyboard", "D");
-        item.insert("xray", "X Ray", "L");
-        item.insert("stuffs", "Other Stuffs", "Y");
-        assertEquals(3, item.count().intValue());
+        item.insert("keyb", "Keyboard", String.valueOf(R.string.zone_admin));
+        item.insert("xray", "X Ray", String.valueOf(R.string.zone_lab));
+        item.insert("paper", "Papers", String.valueOf(R.string.zone_deposit));
+        item.insert("stuffs", "Other Stuffs", String.valueOf(R.string.zone_both));
+        assertEquals(4, item.count().intValue());
 
-        item.update("stuffs", "X Rayed Keyboard", "Y");
+        item.update("stuffs", "X Rayed Keyboard", String.valueOf(R.string.zone_both));
         Item stuff = ItemManager.load(getContext(), "stuffs");
         assertEquals("stuffs", stuff.getCode());
         assertEquals("X Rayed Keyboard", stuff.getName());
 
         item.delete("stuffs");
-        assertEquals(2, item.count().intValue());
+        assertEquals(3, item.count().intValue());
     }
 
     public void testStock() throws Exception {
-        stock.insert(1, "A", "C", "ancho", 1);
-        stock.insert(2, "D", "A", "alex", 2);
-        stock.insert(3, "L", "C", "ondina", 2);
-        stock.insert(4, "Y", "A", "ondina", 3);
-        stock.insert(5, "Y", "C", "ancho", 4);
+        stock.insert(1, String.valueOf(R.string.zone_admin), String.valueOf(R.string.stock_active), "ancho", 1);
+        stock.insert(2, String.valueOf(R.string.zone_deposit), String.valueOf(R.string.stock_active), "alex", 2);
+        stock.insert(3, String.valueOf(R.string.zone_lab), String.valueOf(R.string.stock_confirmed), "ondina", 2);
+        stock.insert(4, String.valueOf(R.string.zone_both), String.valueOf(R.string.stock_active), "ondina", 3);
+        stock.insert(5, String.valueOf(R.string.zone_both), String.valueOf(R.string.stock_confirmed), "ancho", 4);
         assertEquals(5, stock.count().intValue());
 
-        /*stock.update("stuffs", "X Rayed Keyboard", "Y");
-        Item stuff = ItemManager.load(getContext(), "stuffs");
-        assertEquals("stuffs", stuff.getCode());
-        assertEquals("X Rayed Keyboard", stuff.getName());
+        stock.update(4, String.valueOf(R.string.zone_admin), String.valueOf(R.string.stock_confirmed), "ancho", 1);
+        Stock stockNr4 = StockManager.load(getContext(), 4);
+        assertEquals(String.valueOf(R.string.stock_confirmed), stockNr4.getStatus());
 
-        stock.delete("stuffs");
-        assertEquals(2, stock.count().intValue());*/
+        stock.delete(5);
+        assertEquals(4, stock.count().intValue());
+    }
+
+    public void testStockDetail() throws Exception{
+
+        stockDetail.insert(1, 1, "keyb", 20f);
+        stockDetail.insert(1, 2, "xray", 3f);
+        stockDetail.insert(2, 1, "paper", 1000f);
+        stockDetail.insert(3, 1, "xray", 10.5f);
+        stockDetail.insert(3, 2, "xray", 2f);
+        stockDetail.insert(3, 3, "stuffs", 4000f);
+        assertEquals(6, stockDetail.count().intValue());
+
+        stockDetail.update(1, 1, "keyb", 30f);
+        StockDetail keybStockDetail = StockDetailManager.load(getContext(), 1, 1);
+        assertEquals(30f, keybStockDetail.getQty());
+
+        stockDetail.delete(1);
+        assertEquals(4, stockDetail.count().intValue());
     }
 }
