6c9248851c419041c4bf2505e1dc58e2dbd37fa7
Carlos Gomez <ancho.corp@gmail.com>
2015-07-07 21:21:26 -0400

    api22 lock screen issue with espresso and travis ci disabling keyguard

--- app/src/main/java/com/geotechpy/geostock/MainActivity.java
+++ app/src/main/java/com/geotechpy/geostock/MainActivity.java
@@ -14,6 +14,7 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -35,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (BuildConfig.DEBUG){ //api22 lock screen issue with espresso and travis ci
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        }
     }
 
     @Override
