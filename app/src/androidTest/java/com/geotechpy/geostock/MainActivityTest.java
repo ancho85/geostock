5b1186cc31deeeb66d1f8d3ecf2b6d32658cb7ba
Carlos Gomez <ancho.corp@gmail.com>
2015-07-13 00:44:20 -0400

    shouldButtonDeleteDatabase test added to mainActivity

--- app/src/androidTest/java/com/geotechpy/geostock/MainActivityTest.java
+++ app/src/androidTest/java/com/geotechpy/geostock/MainActivityTest.java
@@ -1,7 +1,6 @@
 package com.geotechpy.geostock;
 
 
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
@@ -9,8 +8,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Login activity test case
@@ -19,13 +24,27 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    @Rule public final ActivityRule<MainActivity> main = new ActivityRule<>(MainActivity.class);
+    @Rule
+    public final ActivityRule<MainActivity> main = new ActivityRule<>(MainActivity.class);
 
     @Test
-    public void shouldBeAbleToLaunchMainScreen(){
-        onView(withText(R.string.username)).check(ViewAssertions.matches(isDisplayed()));
-        onView(withText(R.string.password)).check(ViewAssertions.matches(isDisplayed()));
+    public void shouldBeAbleToLaunchMainScreen() {
+        onView(withText(R.string.username)).check(matches(isDisplayed()));
+        onView(withText(R.string.password)).check(matches(isDisplayed()));
 
+    }
+
+    @Test
+    public void shouldButtonDeleteDatabase() {
+        onView(withId(R.id.btn_reset)).perform(click());
+        onView(withText(R.string.confirm_title)).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button1)).perform(click());
+
+        //Toast text
+        onView(withText(R.string.db_reset)
+            ).inRoot(withDecorView(
+                not(main.get().getWindow().getDecorView())
+                )).check(matches(isDisplayed()));
     }
 
 }
