d4e0c915347f3e578c676ee8d49e0bd6584a847a
Carlos Gomez <ancho.corp@gmail.com>
2015-07-18 18:43:22 -0400

    stock type activity for user deposit and laboratory

--- app/src/androidTest/java/com/geotechpy/geostock/MainActivity2Test.java
+++ app/src/androidTest/java/com/geotechpy/geostock/MainActivity2Test.java
@@ -6,9 +6,7 @@ import android.test.suitebuilder.annotation.LargeTest;
 import com.geotechpy.geostock.resources.VolleyIdlingResource;
 import com.geotechpy.geostock.rules.ActivityRule;
 
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,7 +20,6 @@ import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -68,7 +65,7 @@ public class MainActivity2Test {
     }
 
     @Test
-    public void shouldDisplayStockTypeOnUserLogin() throws InterruptedException{
+    public void shouldDisplayStockTypeDepositOnUserLogin() throws InterruptedException{
         onView(withId(R.id.et_user)).perform(clearText(), typeText("ancho"), closeSoftKeyboard());
         Thread.sleep(2000);
         onView(withId(R.id.et_password)).perform(clearText(), typeText("777"), closeSoftKeyboard());
@@ -83,9 +80,30 @@ public class MainActivity2Test {
         onView(withId(R.id.btn_laboratory)).check(matches(not(isDisplayed())));
         onView(withText("ancho")).check(matches(isDisplayed()));
 
+        onView(withId(R.id.btn_deposit)).perform(click());
+        onView(withId(R.id.tv_stocklist_username)).check(matches(isDisplayed()));
+
+        pressBack();
         pressBack();
         onView(withText(R.string.confirm_exit)).check(matches(isDisplayed()));
         onView(withId(android.R.id.button1)).perform(click());
 
+    }
+
+    @Test
+    public void shouldDisplayStockTypeLaboratoryOnUserLogin() throws InterruptedException{
+        onView(withId(R.id.et_user)).perform(clearText(), typeText("alex"), closeSoftKeyboard());
+        Thread.sleep(2000);
+        onView(withId(R.id.et_password)).perform(clearText(), typeText("777"), closeSoftKeyboard());
+        Thread.sleep(2000);
+        onView(withText(R.string.login)).perform(click());
+        onView(withId(R.id.btn_laboratory)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_deposit)).check(matches(not(isDisplayed())));
+        onView(withText("alex")).check(matches(isDisplayed()));
+
+        pressBack();
+        onView(withText(R.string.confirm_exit)).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button2)).perform(click());
+        onView(withText(R.id.tv_stocktype_username)).check(matches(isDisplayed()));
     }
 }
