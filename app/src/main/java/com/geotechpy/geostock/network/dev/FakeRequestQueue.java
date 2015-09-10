5140256f70c3ee405f7051a54c152266f640d6f3
Carlos Gomez <ancho.corp@gmail.com>
2015-09-09 23:32:16 -0400

    removed logs

--- app/src/main/java/com/geotechpy/geostock/network/dev/FakeRequestQueue.java
+++ app/src/main/java/com/geotechpy/geostock/network/dev/FakeRequestQueue.java
@@ -1,15 +1,11 @@
 package com.geotechpy.geostock.network.dev;
 
 import android.content.Context;
-import android.util.Log;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.NoCache;
-
-import java.io.UnsupportedEncodingException;
 
 public class FakeRequestQueue extends RequestQueue {
     public FakeRequestQueue(Context context) {
@@ -19,33 +15,16 @@ public class FakeRequestQueue extends RequestQueue {
 
     @Override
     public void start() {
-        Log.d("FAKE", "request start");
         super.start();
     }
 
     @Override
     public void stop() {
-        Log.d("FAKE", "request stop");
         super.stop();
     }
 
     @Override
-    @SuppressWarnings("unchecked")
-    public Request add(Request request) {
-        Log.w("FAKE", "Note: FakeRequestQueue is used");
-        Log.d("FAKE",
-                "New request " + request.getUrl() + " is added with priority " + request.getPriority());
-        try {
-            if (request.getBody() == null) {
-                Log.d("FAKE", "body is null");
-            } else {
-                Log.d("FAKE", "Body:" + new String(request.getBody(), "UTF-8"));
-            }
-        } catch (AuthFailureError e) {
-            // cannot do anything
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+    public <T> Request<T> add(Request<T> request) {
         return super.add(request);
     }
 }
